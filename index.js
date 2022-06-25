181ada6589d9cb05d2fa1384860c126b4d8307f5
defnotad <aditya_201800208@smit.smu.edu.in>
2022-06-25 16:43:27 +0530

    HTTPS config, Success and Failure pages

--- index.js
+++ index.js
@@ -1,3 +1,95 @@
+// // Packages
+
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const admin = require('firebase-admin');
+// const fs = require("fs");
+// const https = require("https");
+// const http = require("http");
+
+
+// // SSL
+
+// const cert = fs.readFileSync(__dirname + '/SSL/getrebate_in.crt');
+// const ca = fs.readFileSync(__dirname + '/SSL/getrebate_in.ca-bundle');
+// const key = fs.readFileSync(__dirname + '/SSL/getrebate_in.key');
+
+// // Create app
+
+// const hostname = 'www.getrebate.in';
+// // const httpsPort = 443;
+
+// const app = express();
+
+// // Creating the https server
+
+// const options = {
+//     cert: cert,
+//     ca: ca,
+//     key: key
+// };
+
+// // const httpsServer = https.createServer(options, app);
+// const httpServer = http.createServer(app);
+
+
+// // Firebase related
+
+// const serviceAccount = require('./serviceAccountKey.json');
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
+
+// const db = admin.firestore();
+// const signups = db.collection('signups');
+
+// const path = __dirname + '/Public/';
+
+// app.use(express.static(path));
+// app.use(express.static(path + 'Assets/'));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', async function (req, res) {
+//     await signups.get().then(function (snapshot) {
+//         console.log(snapshot.size);
+//     });
+//     res.sendFile(path + 'index.html');
+// });
+
+// app.post('/', async function (req, res) {
+//     console.log("started");
+//     email = req.body.email;
+//     mobile = '+91' + req.body.mobile;
+//     if (email == '' && mobile == '+91' && mobile.length != 12) {
+//         alert("Check details");
+//     }
+//     const check = await signups.where('mobile', '==', mobile).get();
+
+//     if (check.empty) {
+//         await signups.add({
+//             mobile: mobile,
+//             email: email,
+//             date: Date(),
+//         }).then(function () {
+//             alert("Done boys");
+//         });
+//     } else {
+//         alert("Something went wrong");
+//     }
+// });
+
+// app.post("/failure", function (req, res) {
+//     res.redirect("/");
+// });
+
+
+// // httpsServer.listen(httpsPort, hostname);
+// app.listen(8080, function () { });
+
 // Packages
 
 const express = require('express');
@@ -6,8 +98,6 @@ const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const fs = require("fs");
 const https = require("https");
-const http = require("http");
-
 
 // SSL
 
@@ -18,7 +108,7 @@ const key = fs.readFileSync(__dirname + '/SSL/getrebate_in.key');
 // Create app
 
 const hostname = 'www.getrebate.in';
-// const httpsPort = 443;
+const httpsPort = 443;
 
 const app = express();
 
@@ -30,9 +120,7 @@ const options = {
     key: key
 };
 
-// const httpsServer = https.createServer(options, app);
-const httpServer = http.createServer(app);
-
+const httpsServer = https.createServer(options, app);
 
 // Firebase related
 
@@ -48,8 +136,7 @@ const signups = db.collection('signups');
 const path = __dirname + '/Public/';
 
 app.use(express.static(path));
-app.use(express.static(path + 'Assets/'));
-
+app.use(express.static(path + 'assets/images/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -61,11 +148,10 @@ app.get('/', async function (req, res) {
 });
 
 app.post('/', async function (req, res) {
-    console.log("started");
     email = req.body.email;
     mobile = '+91' + req.body.mobile;
     if (email == '' && mobile == '+91' && mobile.length != 12) {
-        alert("Check details");
+        res.sendFile(__dirname + 'failure.html');
     }
     const check = await signups.where('mobile', '==', mobile).get();
 
@@ -75,10 +161,10 @@ app.post('/', async function (req, res) {
             email: email,
             date: Date(),
         }).then(function () {
-            alert("Done boys");
+            res.sendFile(__dirname + 'success.html');
         });
     } else {
-        alert("Something went wrong");
+        res.sendFile(__dirname + 'failure.html');
     }
 });
 
@@ -87,5 +173,5 @@ app.post("/failure", function (req, res) {
 });
 
 
-// httpsServer.listen(httpsPort, hostname);
-app.listen(8080, function () { });
+httpsServer.listen(httpsPort, hostname);
+// app.listen(8080, function () {});
