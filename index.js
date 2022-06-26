a4748db565234bf81ccd3cfc01da7483434378fd
defnotad <aditya_201800208@smit.smu.edu.in>
2022-06-26 15:39:36 +0530

    Experimenting with http

--- index.js
+++ index.js
@@ -97,30 +97,41 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const fs = require("fs");
-const https = require("https");
+// const https = require("https");
+const http = require("http");
 
 // SSL
 
-const cert = fs.readFileSync(__dirname + '/SSL/getrebate_in.crt');
-const ca = fs.readFileSync(__dirname + '/SSL/getrebate_in.ca-bundle');
-const key = fs.readFileSync(__dirname + '/SSL/getrebate_in.key');
+// const cert = fs.readFileSync(__dirname + '/SSL/getrebate_in.crt');
+// const ca = fs.readFileSync(__dirname + '/SSL/getrebate_in.ca-bundle');
+// const key = fs.readFileSync(__dirname + '/SSL/getrebate_in.key');
 
 // Create app
 
 const hostname = 'www.getrebate.in';
-const httpsPort = 443;
+// const httpsPort = 443;
+const port = 3000;
 
 const app = express();
 
 // Creating the https server
 
-const options = {
-    cert: cert,
-    ca: ca,
-    key: key
-};
+// const options = {
+//     cert: cert,
+//     ca: ca,
+//     key: key
+// };
 
-const httpsServer = https.createServer(options, app);
+// const httpsServer = https.createServer(options, app);
+
+// Redirect http requests
+
+// app.use((req, res, next) => {
+//     if(req.protocol === 'http') {
+//       res.redirect(301, `https://${req.headers.host}${req.url}`);
+//     }
+//     next();
+//  });
 
 // Firebase related
 
@@ -132,6 +143,8 @@ admin.initializeApp({
 
 const db = admin.firestore();
 const signups = db.collection('signups');
+
+// Begin
 
 const path = __dirname + '/Public/';
 
@@ -173,5 +186,14 @@ app.post("/failure", function (req, res) {
 });
 
 
-httpsServer.listen(httpsPort, hostname);
+// httpsServer.listen(httpsPort, hostname);
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 // app.listen(8080, function () {});
